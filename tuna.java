7d025ef502f42abb8408dc0b4e144afc1077e8c3
Flamelynx <60495535+Flamelynx@users.noreply.github.com>
2020-07-27 13:21:38 -0400

    Create tuna.java
    I was following a tutorial before I made this whole code, so I didn't bother to change the file name from tuna.java

---
+++ tuna.java
@@ -0,0 +1,43 @@
+public class tuna {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	public void setTime(int h, int m, int s) {
+		
+		//hours check
+		if (h >=0 && h<24) {
+			hour = h;
+		}
+		else {
+			System.out.println("That is not a valid time!");
+			System.exit(0);
+		}
+		
+		//minutes now
+		if (m >=0 && m<60) {
+			minute = m;
+		}
+		else {
+			System.out.println("That is not a valid time!");
+			System.exit(0);
+		}	
+		
+		//seconds now
+		if (s >=0 && s<60) {
+			second = s;
+		}
+		else {
+			System.out.println("That is not a valid time!");
+			System.exit(0);
+		}		
+	}
+	
+	public String toMilitary(){
+		return String.format("%02d:%02d:%02d", hour, minute, second);
+		
+	}
+	
+	
+	
+}
